59917fccb1a725d04cc764eed3c915f82593233e
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-12 16:06:04 +0200

    finnnnnnish

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.DateTimeField(auto_now_add=True)
-    closing_time = models.CharField(max_length=120)
+    closing_time = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
